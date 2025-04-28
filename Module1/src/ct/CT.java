0e06c9dbe6be3153b2ce080fad404af926e29e1d
DGG3211 <daniel.gallegos@csuglobal.edu>
2025-04-28 03:44:22 +0200

    module 1 critical thinking

---
+++ Module1/src/ct/CT.java
@@ -0,0 +1,10 @@
+package ct;
+public class CT {
+public static void main(String[] args) {
+	     System.out.println("Danny");
+	        System.out.println("G");
+	        System.out.println("Via Giambattista Marino");
+	        System.out.println("Napoli");
+	        System.out.println("80125");
+	    }
+	}
